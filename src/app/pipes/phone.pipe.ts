2c454cc3e4ad8b7ba67c0119d30d3afa6a51196d
Pritesh Patel <3ppatel@gmail.com>
2016-10-22 14:55:05 -0400

    fixed phone pile

--- src/app/pipes/phone.pipe.ts
+++ src/app/pipes/phone.pipe.ts
@@ -5,13 +5,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class PhonePipe implements PipeTransform {
     transform(val, args) {
-        //return '(' + val + ')';
-        let numbers = val.value.Replace(/\D/g, ''),
-        char = { 0: '(', 3: ') ', 6: ' - ' };
-        val.value = '';
-        for (var i = 0; i < numbers.length; i++) {
-            val.value += (char[i] || '') + numbers[i];
-        }
-        return val;
+       return '(' + val + ')';
     }
 }
